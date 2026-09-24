3e8790dda3cb73804c01b37c85196ee3910f40e5
SiERRA74 <aksierra74@gmail.com>
2026-09-24 22:32:01 +0200

    music player working, marquee title working too
    i need to probably use PHP so that i can save the playing/pausing states and make the text stop spining, same for the sound waves gif

--- music.js
+++ music.js
@@ -1,17 +1,28 @@
-// Switch between pause/play button appearance ▶︎ ❚❚
-$(".playy").click(function() { $(".pausee").show(); $(".playy").hide(); });
+$(document).ready(function(){
+    $(".playy").click(function() {
+        $(".pausee").show();
+        $(".playy").hide();
+    });
 
-$(".pausee").click(function() { $(".playy").show(); $(".pausee").hide(); });
-
+    $(".pausee").click(function() {
+        $(".playy").show();
+        $(".pausee").hide();
+    });
+});
 
 function songstart() {
-    var music = document.getElementById("tune");
-    if (music.paused) music.play();
-    else music.pause();
+var harmonia = document.getElementById("tune");
+    if (harmonia.paused) {
+        harmonia.play();
+    } else { 
+        harmonia.pause();
+    }
 }
 
-// When the song ends, reset the button back to ►
 $(document).ready(function(){
     var owari = document.getElementById("tune");
-    owari.onended = function() { $(".playy").show(); $(".pausee").hide(); };
+    owari.onended = function() {
+        $(".playy").show();
+        $(".pausee").hide();
+    };
 });
